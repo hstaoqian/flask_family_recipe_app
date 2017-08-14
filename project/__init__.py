d075b1b6fb6f442f39c6196ec1ad3061ab55c498
hstaoqian <hstaoqian@me.com>
2017-08-14 15:35:17 +0800

    Add SQLALCHEMY support and file for initiallizing database

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
+
+db = SQLAlchemy(app)
 
 from project.users.views import users_blueprint
 from project.recipes.views import recipes_blueprint

52c94b78aa530eb01739396820d34ad7a40e9e50
hstaoqian <hstaoqian@me.com>
2017-08-14 14:55:36 +0800

    Added use of Blueprints to organize project

---
+++ project/recipes/views.py
@@ -0,0 +1,7 @@
+from flask import render_template, Blueprint
+
+recipes_blueprint = Blueprint('recipes', __name__, template_folder='templates')
+
+@recipes_blueprint.route('/')
+def index():
+    return render_template('index.html')
---
+++ project/tests/test_users.py
@@ -0,0 +1,21 @@
+import unittest
+from project import app
+
+
+class ProjectTests(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+        self.assertEquals(app.debug, False)
+
+    def tearDown(self):
+        pass
+
+    def test_login_page(self):
+        response = self.app.get('/login', follow_redirects=True)
+        self.assertIn(b'Future site for logging into Kennedy Family Recipes!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()

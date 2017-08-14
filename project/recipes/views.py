2f360f136811324c1b930fe3354e03fb9614e134
hstaoqian <hstaoqian@me.com>
2017-08-14 16:17:42 +0800

    Added use of templates and Bootstrap

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -1,7 +1,9 @@
 from flask import render_template, Blueprint
+from project.models import Recipe
 
 recipes_blueprint = Blueprint('recipes', __name__, template_folder='templates')
 
 @recipes_blueprint.route('/')
 def index():
-    return render_template('index.html')
+    all_recipes = Recipe.query.all()
+    return render_template('recipes.html', recipes=all_recipes)

07df087a5a84a6400c1bd4d2b4690c02e4001de7
hstaoqian <hstaoqian@me.com>
2017-08-23 22:11:25 +0800

    Added custom error pages for 404, 403 and 410 error codes

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
@@ -22,6 +22,21 @@ from project.models import User
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.filter(User.id == int(user_id)).first()
+
+# custom error pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(410)
+def page_not_found(e):
+    return render_template('410.html'), 410
 
 # blueprints
 
